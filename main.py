772f86215120c1f8679f56fae2d74ec0d87549cd
артемий <sk0opy@yandex.ru>
2025-12-15 21:28:08 +0300

    начальный фон, картинка к нему

--- main.py
+++ main.py
@@ -2,8 +2,30 @@ import arcade
 import math
 import random
 
+WIDTH = 1300
+HEIGHT = 900
+TITLE = 'Wyvern: The Path to the Crown of Heaven'
 
-class Game(arcade.Window):
-    pass
+
+class Start_menu(arcade.Window):
+    def __init__(self, width, height, title):
+        size = arcade.get_display_size()
+        if isinstance(size, tuple):
+            self.w = size[0]
+            self.h = size[1]
+        super().__init__(width, height, title, fullscreen=True)
+        self.texture = arcade.load_texture('images/backgrounds/start_menu.jpg')
+
+    def on_draw(self):
+        self.clear()
+
+        arcade.draw_texture_rect(self.texture, arcade.rect.XYWH(self.w // 2, self.h // 2, self.w, self.h))
 
 
+def main():
+    game = Start_menu(WIDTH, HEIGHT, TITLE)
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
