e85c11968304678f4010180ef63f0a1c0be0a454
артемий <sk0opy@yandex.ru>
2025-12-16 16:37:28 +0300

    добавлены частицы листьев на главный экран, шрифт не работает

--- main.py
+++ main.py
@@ -11,21 +11,61 @@ TITLE = 'Wyvern: The Path to the Crown of Heaven'
 class Start_menu(arcade.Window):
     def __init__(self, width, height, title):
         size = arcade.get_display_size()
+
         if isinstance(size, tuple):
             self.w = size[0]
             self.h = size[1]
+
         super().__init__(width, height, title, fullscreen=True)
+
         self.texture = arcade.load_texture('images/backgrounds/start_menu.jpg')
-        # self.texture_title = arcade.load_texture('images/backgrounds/title.jpg')
         arcade.load_font('fonts/Comic Sans MS Pixel/Comic Sans MS Pixel.ttf')
 
-    def on_draw(self):
-        self.clear()
+        self.parcticles = []
+        for i in range(350):
+            self.parcticles.append({
+                'x': random.uniform(0, self.w),
+                'y': random.uniform(0, self.h),
+                'size': random.uniform(2, 8),
+                'speed': random.uniform(5.5, 10.5),
+                'color': random.choice([
+                    (255, 192, 203, 150),  # Розовый (лепестки сакуры)
+                    (255, 182, 193, 150),  # Светло-розовый
+                    (255, 160, 122, 150),  # Светло-коралловый
+                    (255, 218, 185, 150),  # Персиковый
+                    (240, 230, 140, 150)
+                ]),
+                'side_speed': random.uniform(-4, 4),
+                'rotation': random.uniform(0,360),
+                'rot_speed': random.uniform(-10, 10)
+            }
+            )
 
+        self.text_main = arcade.Text('Wyvern: The Path to the Crown of Heaven', self.w // 2, self.h * 0.8, arcade.color.APRICOT,
+                         font_size=38, font_name="Comic Sans MS Pixel.ttf", anchor_x='center', anchor_y='top')
+
+    def on_update(self, delta_time):
+        for i in self.parcticles:
+            i['y'] -= i['speed'] * delta_time
+            i['x'] += i['side_speed'] * delta_time
+            i['rotation'] += i['rot_speed'] * delta_time
+
+            if i['y'] <= 0:
+                i['y'] = self.h + 10
+            if i['x'] <= -10:
+                i['x'] = self.w + 10
+            elif i['x'] >= self.w + 10:
+                i['x'] = -10
+
+    def on_draw(self):
         arcade.draw_texture_rect(self.texture, arcade.rect.XYWH(self.w // 2, self.h // 2, self.w, self.h))
         # arcade.draw_texture_rect(self.texture_title, arcade.XYWH(self.w // 2, self.h - 30, self.w // 1.1, self.h // 3.2))
-        arcade.draw_text('Wyvern: The Path to the Crown of Heaven', self.w // 7, self.h - 50, arcade.color.NAVAJO_WHITE,
-                         font_size=38, font_name='Comic Sans MS Pixel')
+
+        self.text_main.draw()
+
+        for i in self.parcticles:
+            arcade.draw_rect_filled(arcade.XYWH(i['x'], i['y'], i['size'], i['size']), i['color'], i['rotation'])
+
 
 
 def main():
