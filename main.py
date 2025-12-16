b154fd60874d02d17da7487ef4ecdb405a81b86c
артемий <sk0opy@yandex.ru>
2025-12-16 19:46:42 +0300

    пробные изменения курсора

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import arcade
 import math
 import random
 
+from pyglet.event import EVENT_HANDLE_STATE
 
-WIDTH = 1300
-HEIGHT = 900
+WIDTH = 5120
+HEIGHT = 2580
 TITLE = 'Wyvern: The Path to the Crown of Heaven'
 
 
@@ -19,10 +20,19 @@ class Start_menu(arcade.Window):
         super().__init__(self.w, self.h, TITLE, fullscreen=True)
 
         self.texture = arcade.load_texture('images/backgrounds/start_menu.jpg')
-        arcade.load_font('C:/Users/User/PycharmProjects/project_game/fonts/Comic Sans MS Pixel/Comic Sans MS Pixel.ttf')
+        arcade.load_font('fonts/Comic Sans MS Pixel/Comic Sans MS Pixel.ttf')
         self.background_sound = arcade.load_sound('sounds/Flappy Dragon - Wispernalia.mp3')
 
-        arcade.play_sound(self.background_sound, loop=True, volume=0.6)
+        self.set_mouse_visible(False)
+        # self.painter_cur = arcade.Sprite('images/cursors/modest-dark/Windows/pointer.cur')
+#        self.link_cur = arcade.Sprite('images/cursors/modest-dark/Windows/link.cur')
+        self.painter_cur = arcade.load_texture('images/cursors/modest-dark/Windows/pointer.cur')
+        self.cursor_w = self.painter_cur.width
+        self.cursor_h = self.painter_cur.height
+        self.cursor_x = self.w // 2
+        self.cursor_y = self.h // 2
+
+        arcade.play_sound(self.background_sound, loop=True, volume=0.1)
 
         self.parcticles = []
         for i in range(350):
@@ -46,6 +56,8 @@ class Start_menu(arcade.Window):
 
         self.text_main = arcade.Text('Wyvern: The Path to the Crown of Heaven', self.w // 2, self.h * 0.8, arcade.color.APRICOT,
                          font_size=46, font_name="Comic Sans MS pixel rus eng", anchor_x='center', anchor_y='top')
+    def on_setup(self):
+        pass
 
     def on_update(self, delta_time):
         for i in self.parcticles:
@@ -67,7 +79,13 @@ class Start_menu(arcade.Window):
         self.text_main.draw()
 
         for i in self.parcticles:
-            arcade.draw_rect_filled(arcade.XYWH(i['x'], i['y'], i['size'], i['size']), i['color'], i['rotation'])
+            arcade.draw_rect_filled(arcade.rect.XYWH(i['x'], i['y'], i['size'], i['size']), i['color'], i['rotation'])
+
+        arcade.draw_texture_rect(self.painter_cur, arcade.rect.XYWH(self.cursor_x, self.cursor_y, self.cursor_w, self.cursor_h))
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.cursor_x = x
+        self.cursor_y = y
 
 
 
